b2c2088cf27f5f649cc9af169c5b68d2938d91e7
Gregory Romantsov <imoonserk@gmail.com>
2021-05-05 10:32:32 +0400

    4.2 add controllers module

--- index.js
+++ index.js
@@ -5,32 +5,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./src/models/blog')
+
+const blogsRouter = require('./src/controllers/blogs')
 const logger = require('./src/utils/logger')
 const config = require('./src/utils/config')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
+
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
