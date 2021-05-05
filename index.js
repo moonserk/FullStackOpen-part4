fd0ed8d7b15552c52e4274d57c6470fc4fe52e1d
Gregory Romantsov <imoonserk@gmail.com>
2021-05-05 10:37:54 +0400

    4.2 add middleware module

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./src/controllers/blogs')
 const logger = require('./src/utils/logger')
 const config = require('./src/utils/config')
+const middleware = require('./src/utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -16,6 +17,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.requestLogger)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
