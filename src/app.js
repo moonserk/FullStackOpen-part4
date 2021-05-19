bffe96a820e1402a3636f9ea78fdd0b507a5d3f5
Gregory Romantsov <imoonserk@gmail.com>
2021-05-19 08:55:08 +0400

    4.15: bloglist expansion, step3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
@@ -20,6 +22,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.requestLogger)
 app.use(middleware.errorHandler)
---
+++ src/controllers/users.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const usersRouter = require('express').Router()
+
+usersRouter.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    try {
+        const savedUser = await user.save()
+        response.json(savedUser)
+    } catch (error){
+        next(error)
+    }
+})
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+module.exports = usersRouter
