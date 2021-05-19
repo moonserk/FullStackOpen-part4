61da6bf24c4e9f9bc4288ce9714491a842c79483
Gregory Romantsov <imoonserk@gmail.com>
2021-05-19 21:46:50 +0400

    4.17: bloglist expansion, step5

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -28,9 +28,13 @@ usersRouter.post('/', async (request, response, next) => {
     }
 })
 
-usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
-    response.json(users)
+usersRouter.get('/', async (request, response, next) => {
+    try{
+        const users = await User.find({}).populate('blogs', {url: 1, title: 1, author: 1})
+        response.json(users)
+    } catch(error){
+        next(error)
+    }
 })
 
 module.exports = usersRouter
