46c98786148947ff2fab7329cc58569ed4463c5c
Gregory Romantsov <imoonserk@gmail.com>
2021-05-23 10:08:15 +0400

    4.19: bloglist expansion, step7

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const config = require('../utils/config')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')){
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
     try {
@@ -12,20 +22,27 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-    const body = request.body
-    const user = await User.find({})
-    console.log(body.userId)
-    const blog = new Blog({
-        title: request.body.title,
-        author: request.body.author,
-        url: request.body.url,
-        likes: request.body.likes || 0,
-        user: user[0]._id
-    })
     try{
+        const body = request.body
+        const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(token, config.SECRET)
+        if (!token || !decodedToken.id){
+            return response.status(401).json({error: 'token missing or invalid'})
+        }
+
+        const user = await User.findById(decodedToken.id)
+        console.log(body.userId)
+        const blog = new Blog({
+            title: request.body.title,
+            author: request.body.author,
+            url: request.body.url,
+            likes: request.body.likes || 0,
+            user: user._id
+        })
+
         const savedBlog = await blog.save()
-        user[0].blogs = user[0].blogs.concat(savedBlog._id)
-        await user[0].save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(201).json(blog)
     }catch(error){
         next(error)
